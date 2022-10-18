30f4bb20dd5161bb94ee861fa14498936d7da51a
siny2000 <sundress198@gmail.com>
2022-10-19 02:26:18 +0700

    Connected MongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const { start } = require("repl");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 
@@ -7,9 +7,23 @@ const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://nasa-api:tWFtPThHV4U9jojs@nasacluster.9x8jliy.mongodb.net/nasa?retryWrites=true&w=majority'
+
 const server = http.createServer(app);
 
+mongoose.connection.on('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   await loadPlanetsData();
 
   server.listen(PORT, () => {
